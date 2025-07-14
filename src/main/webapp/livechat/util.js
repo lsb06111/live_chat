414f2fa2d46f8cb2e098558ea485b0fa3829f782
Ben Lee <u6682215@anu.edu.au>
2025-07-14 11:48:48 +0900

    update url

--- src/main/webapp/livechat/util.js
+++ src/main/webapp/livechat/util.js
@@ -45,7 +45,7 @@ function removeVote(idx){
 	console.log(typeof idx);
 	
 	$.ajax({
-		  url: '/ChatApp/chat?type=remove_vote',
+		  url: 'http://localhost:8888/ChatApp/chat?type=remove_vote',
 		  type: 'POST',
 		  contentType: 'application/json',
 		  data: JSON.stringify({content:"", date_info:"", id : idx, vote_total : ""}),
@@ -103,7 +103,7 @@ function doVote(button, date, id, items, status){
 		
 		
 		$.ajax({
-          url: '/ChatApp/chat?type=add_vote',
+          url: 'http://localhost:8888/ChatApp/chat?type=add_vote',
           type: 'POST',
           contentType: 'application/json',
           data: JSON.stringify({content:"", date_info:"", id : id, vote_total : totals}),
